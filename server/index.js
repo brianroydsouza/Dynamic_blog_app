c4a7382ee0308ea7f309f4f31a157fbb4b351b9e
Brian Dsouza <91792234+BrianRoystunDsouza@users.noreply.github.com>
2023-08-01 17:38:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use(cookieParser());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../client/public/upload");
+    cb(null, "./public/upload");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
