d343cf9c61f6b557ae5b3a20a9deac8d2622614f
Brian Dsouza <91792234+BrianRoystunDsouza@users.noreply.github.com>
2023-08-03 00:48:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ import cookieParser from "cookie-parser";
 import mongoose  from "mongoose";
 import cors from "cors"
 import dotenv from 'dotenv';
-import multer from "multer";
-import path from "path";
+import {v2 as cloudinary } from 'cloudinary';
+
 const app = express();
 dotenv.config();
 
@@ -21,6 +21,8 @@ app.use(express.json({ limit: '100mb' }));
 // Increase payload size limit for URL-encoded requests
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
+//static files 
+
 //DATABASE connection
 mongoose
   .connect(process.env.COSMOSDB_URL )
@@ -30,25 +32,13 @@ mongoose
 app.use(express.json());
 app.use(cookieParser());
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // Use an absolute path for the destination directory
-    cb(null, path.join(__dirname, "public", "upload"));
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + file.originalname);
-  },
-});
-
-const upload = multer({ storage });
-
-app.post("/api/upload", upload.single("file"), function (req, res) {
-  const file = req.file;
-  res.status(200).json(file.filename);
-});
-
-// Serve uploaded files as static files
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.listen(port, ()=>console.log("Server Up and running"))
