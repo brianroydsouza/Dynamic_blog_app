47d3c6b592a0d43c9448be4f09641103d9ee30f9
Brian Dsouza <91792234+BrianRoystunDsouza@users.noreply.github.com>
2023-08-03 13:35:52 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(process.env.REACT_APP_BACKEND_URL + 'api/posts',{
+        const res = await axios.get(process.env.REACT_APP_BACKEND_URL + 'api/posts', {
           headers: {
             Authorization: `Bearer ${token}`,
           }
@@ -50,7 +50,7 @@ const Home = () => {
   //   },
   // ];
 
-  const getText = (html) =>{
+  const getText = (html) => {
     const doc = new DOMParser().parseFromString(html, "text/html")
     return doc.body.textContent
   }
@@ -60,17 +60,33 @@ const Home = () => {
         {posts.length > 0 ? posts.map((post) => (
           <div className="post" key={post._id}>
             <div className="img">
-              <img src={post.img} alt="" />
+              {post.imgType === "image" ? (
+                <img src={post?.img} alt="" />
+              ) : post.imgType === "video" ? (
+                <video controls>
+                  <source src={post?.img} type="video/mp4" />
+                  Your browser does not support HTML video.
+                </video>
+              ) :
+                <iframe
+                  width="660"
+                  height="315"
+                  src={`https://www.youtube.com/embed/${post?.img.split('watch?v=')[1]}`}
+                  title="YouTube Video Player"
+                  frameBorder="0"
+                  allowFullScreen
+                ></iframe>
+              }
             </div>
             <div className="content">
-                <h1>{post.title}</h1>
+              <h1>{post.title}</h1>
               <p>{getText(post.desc)}</p>
               <Link className="link" to={`/post/${post._id}`}>
-              <button>Read More</button>
+                <button>Read More</button>
               </Link>
             </div>
           </div>
-        )): <span className="NoPosts">No Posts</span>}
+        )) : <span className="NoPosts">No Posts</span>}
       </div>
     </div>
   );
