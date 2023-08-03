218634788cb339139d28e1bb02f56423fb6b1e90
Brian Dsouza <91792234+BrianRoystunDsouza@users.noreply.github.com>
2023-08-03 14:54:27 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Link , useNavigate} from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../context/authContext";
 import Logo from "../img/logo.png";
 
@@ -11,49 +11,52 @@ const Navbar = () => {
     logout();
     navigate("/")
   };
-
+  const path = useLocation().pathname
 
   return (
     <div className="navbar">
       <div className="container">
         <div className="logo">
+
           <Link to="/">
-          <img src={Logo} alt="" />
+            <img src={Logo} alt="" />
           </Link>
         </div>
-        <div className="links">
-          <Link className="link" >
-            <h6>Travel</h6>
-          </Link>
-          <Link className="link">
-            <h6>Adventure</h6>
-          </Link>
-          <Link className="link">
-            <h6>Health </h6>
-          </Link>
-          <Link className="link">
-            <h6>Technology </h6>
-          </Link>
-          <Link className="link">
-            <h6>Fashion </h6>
-          </Link>
-          <Link className="link">
-            <h6>Food</h6>
-          </Link>
-          <span>{ currentUser && ( <>Hey {currentUser?.user.UserName}</>)}</span>
-          {currentUser ? (
-            <span onClick={handleLogout}>Logout</span>
-          ) : (
-            <Link className="link" to="/login">
-              Login
+        {path == "/" ?
+          <div className="links">
+            <Link className="link" to="/?cat=Travel">
+              <h6>Travel</h6>
             </Link>
-          )}
-         {currentUser?.user.Role === "Admin"?<span className="write">
-            <Link className="link" to="/write">
-              Write
+            <Link className="link" to="/?cat=Adventure">
+              <h6>Adventure</h6>
             </Link>
-          </span>: null}
-        </div>
+            <Link className="link" to="/?cat=Health">
+              <h6>Health </h6>
+            </Link>
+            <Link className="link" to="/?cat=Technology">
+              <h6>Technology </h6>
+            </Link>
+            <Link className="link" to="/?cat=Fashion">
+              <h6>Fashion </h6>
+            </Link>
+            <Link className="link" to="/?cat=food">
+              <h6>Food</h6>
+            </Link>
+            <span>{currentUser && (<>Hey {currentUser?.user.UserName}</>)}</span>
+            {currentUser ? (
+              <span onClick={handleLogout}>Logout</span>
+            ) : (
+              <Link className="link" to="/login">
+                Login
+              </Link>
+            )}
+            {currentUser?.user.Role === "Admin" ? <span className="write">
+              <Link className="link" to="/write">
+                Write
+              </Link>
+            </span> : null}
+          </div>
+          : <></>}
       </div>
     </div>
   );
